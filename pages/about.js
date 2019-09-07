3fcb9acf38fb8c779b203e07ed1be5c2034b8418
Michelle Burton <michelle_burton@bradycorp.com>
2019-09-07 15:36:04 -0500

    First Commit

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../comp/MyLayout';
+
+export default function About() {
+    return (
+        <Layout>
+            <p>This is the about page</p>
+        </Layout>
+    );
+}
